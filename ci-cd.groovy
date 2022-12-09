6d1958d870f70765eb0d128a3bfc8d431d07c87c
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 09:10:35 -0300

    fix

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -1,11 +1,15 @@
 pipeline {
     agent any
 
+    organization = env.ORGANIZATION
+    repository = env.REPOSITORY
+    branch = env.BRANCH
+
     stages {
         stage('clone') {
             steps {
-                withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-                    sh "git clone --branch master --single-branch https://${USERNAME}:${PASSWORD}@github.com/${env.ORGANIZATION}/${env.REPOSITORY}.git ${env.REPOSITORY}"
+                withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
+                    sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$organization/$repository.git $repository'
                     sh "ls"
                 }
             }
