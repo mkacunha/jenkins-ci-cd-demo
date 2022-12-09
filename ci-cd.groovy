9b9e163049c321b210d621546ccf9e8d09c0cde7
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 09:19:15 -0300

    test

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -5,14 +5,26 @@ pipeline {
     def repository = env.REPOSITORY
     def branch = env.BRANCH
 
+    parameters {
+        string(name: 'STATEMENT', defaultValue: 'hello; ls /', description: 'What should I say?')
+    }
+
+    // stages {
+    //     stage('clone') {
+    //         steps {
+    //             withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
+    //                 sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$organization/$repository.git $repository'
+    //                 sh "ls"
+    //             }
+    //         }
+    //     }
+    // }
+
     stages {
         stage('clone') {
-            steps {
-                withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
-                    sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$organization/$repository.git $repository'
-                    sh "ls"
-                }
-            }
+           steps {
+             echo "${STATEMENT}"
+           }
         }
     }
 }
