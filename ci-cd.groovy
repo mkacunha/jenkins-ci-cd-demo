efda6986f6e069825985d1a7dbbc648375984f73
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 09:49:48 -0300

    test

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -1,26 +1,14 @@
 pipeline {
     agent any
 
-    // parameters {
-    //     string(name: 'STATEMENT', defaultValue: 'hello; ls /', description: 'What should I say?')
-    // }
-
-    // stages {
-    //     stage('clone') {
-    //         steps {
-    //             withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
-    //                 sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$organization/$repository.git $repository'
-    //                 sh "ls"
-    //             }
-    //         }
-    //     }
-    // }
-
     stages {
         stage('clone') {
-           steps {
-             echo "oiiiiii"
-           }
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
+                    sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$env.ORAGANIZATION/$env.REPOSITORY.git $env.REPOSITORY'
+                    sh "ls"
+                }
+            }
         }
     }
 }
