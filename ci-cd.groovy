d9c557b9874ea4ac6f5c5f31719059897cf490b5
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 09:50:32 -0300

    test

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage('clone') {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
-                    sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/$env.ORAGANIZATION/$env.REPOSITORY.git $env.REPOSITORY'
+                    sh 'git clone --branch $branch --single-branch https://$username:$password@github.com/${env.ORAGANIZATION}/${env.REPOSITORY}.git ${env.REPOSITORY}'
                     sh "ls"
                 }
             }
