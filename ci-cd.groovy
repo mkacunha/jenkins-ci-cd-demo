d561567a614e6e40714765302936e5b0fcdc14aa
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 10:44:54 -0300

    test

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -1,9 +1,73 @@
 pipeline {
     agent any
-    node() {
-        stages {
-            stage('foi') {
-                echo 'foiii'
+
+    stages {
+        stage('clone') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'github-mkacunha',  usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                    sh 'rm -rf gradle-release-tag-demo'
+                    sh 'git clone --branch $BRANCH --single-branch https://$USERNAME:$PASSWORD@github.com/$ORGANIZATION/$REPOSITORY.git $REPOSITORY'
+                }
+            }
+        }
+
+        stage('check pull request') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('tag') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('build docker image') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('deploy QA') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('QA acceptance test') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('deploy SANDBOX') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('SANDBOX acceptance test') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('deploy PROD') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('PROD acceptance test') {
+            steps {
+                echo 'foi'
+            }
+        }
+
+        stage('complete pull request') {
+            steps {
+                echo 'foi'
             }
         }
     }
