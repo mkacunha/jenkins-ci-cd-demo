560ed26642921244a797988812a889b66d1ff9b5
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 08:54:24 -0300

    test parameters

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -1,32 +1,12 @@
 pipeline {
     agent any
 
-    environment {
-        PULL_REQUEST_BUILD_TYPE = 'PULL REQUEST'
-        DELIVERY_BUILD_TYPE = 'DELIVERY'
-    }
-
-    if ("$PULL_REQUEST_BUILD_TYPE" == "${env.TYPE}") {
-        stages {
-
-            stage('first') {
-                steps {
-                    echo 'Hello world pull request'
-                    echo "${env.TYPE}"
-                    echo "${status.STARTED}"
-                }
-            }
-        }
-    }
-
-    if ("$DELIVERY_BUILD_TYPE" == "$env.TYPE") {
-        stages {
-            stage('first') {
-                steps {
-                    echo 'Hello world delivery'
-                    echo "${env.TYPE}"
-                    echo "${status.STARTED}"
-                }
+    stages {
+        stage('first') {
+            steps {
+                echo 'Hello world pull request'
+                echo "${env.ORGANIZATION}"
+                echo "${status.REPOSITORY}"
             }
         }
     }
