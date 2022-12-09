fc5dd83fa9df20dbfa10800a9e994fd981dd9021
Maiko Cunha <maicunha@uolinc.com>
2022-12-09 09:57:53 -0300

    test

--- ci-cd.groovy
+++ ci-cd.groovy
@@ -6,7 +6,7 @@ pipeline {
             steps {
                 withCredentials([usernamePassword(credentialsId: 'github-mkacunha', passwordVariable: 'password', usernameVariable: 'username')]) {
                     sh 'rm -r gradle-release-tag-demo'
-                    sh 'git clone --branch master --single-branch https://$username:$password@github.com/mkacunha/gradle-release-tag-demo.git gradle-release-tag-demo'
+                    sh 'git clone --branch ${env.BRANCH} --single-branch https://$username:$password@github.com/mkacunha/gradle-release-tag-demo.git gradle-release-tag-demo'
                     sh "ls"
                 }
             }
